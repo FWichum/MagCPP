d3fee063c2dd94b47ea7af6e6e740ddb3f575dad
FelixW <35849448+FelixW@users.noreply.github.com>
2019-07-03 15:06:17 +0200

    Sceletal Structure
    void definition of all methods

--- magstim.cpp
+++ magstim.cpp
@@ -4,3 +4,93 @@ MagStim::MagStim()
 {
 
 }
+
+void MagStim::parseMagstimResponse()
+{
+
+}
+
+void MagStim::connect()
+{
+
+}
+
+void MagStim::disconnect()
+{
+
+}
+
+void MagStim::remoteControl()
+{
+
+}
+
+void MagStim::getParameters()
+{
+
+}
+
+void MagStim::setPower()
+{
+
+}
+
+void MagStim::getTemperature()
+{
+
+}
+
+void MagStim::poke()
+{
+
+}
+
+void MagStim::arm()
+{
+
+}
+
+void MagStim::disarm()
+{
+
+}
+
+void MagStim::isArmed()
+{
+
+}
+
+void MagStim::isUnderControl()
+{
+
+}
+
+void MagStim::isReadyToFire()
+{
+
+}
+
+void MagStim::fire()
+{
+
+}
+
+void MagStim::resetQuickFire()
+{
+
+}
+
+void MagStim::quickFire()
+{
+
+}
+
+void MagStim::setupSerialPort()
+{
+
+}
+
+void MagStim::processCommand()
+{
+
+}
--- magstim.h
+++ magstim.h
@@ -6,6 +6,27 @@ class MagStim
 {
 public:
     MagStim();
+    void parseMagstimResponse();
+    void connect();
+    void disconnect();
+    void remoteControl();
+    void getParameters();
+    void setPower();
+    void getTemperature();
+    void poke();
+    void arm();
+    void disarm();
+    void isArmed();
+    void isUnderControl();
+    void isReadyToFire();
+    void fire();
+    void resetQuickFire();
+    void quickFire();
+
+
+private:
+    void setupSerialPort();
+    void processCommand();
 };
 
 #endif // MAGSTIM_H
