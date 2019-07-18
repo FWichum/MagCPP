6be02d6d7de4f174851ad9c8b88b1718c414fb0a
FelixW <35849448+FelixW@users.noreply.github.com>
2019-07-18 11:35:08 +0200

    Add constructor
    TODO: read from yaml, unlockCode

--- rapid.cpp
+++ rapid.cpp
@@ -1,6 +1,24 @@
 #include "rapid.h"
 
-Rapid::Rapid()
+Rapid::Rapid(QString serialConnection, bool superRapid, QString unlockCode, int voltage, std::tuple<int, int, int> version) :
+    MagStim(serialConnection)
+{
+    this->super = superRapid;
+    this->unlockCode = unlockCode;
+    this->voltage = voltage;
+    if (serialConnection.toLower() == "virtual") {
+        this->version = version; //FW: Is it right?
+    } else {
+        this->version = std::make_tuple(0,0,0); // FW: Is it right to set to (0,0,0)?
+    }
+    // TODO UnlockCode
+    this->parameterReturnBytes = NAN;
+    this->sequenceValidated = false;
+    this->repetitiveMode = false;
+}
+
+Rapid::Rapid(QString serialConnection) :
+    MagStim(serialConnection)
 {
 
 }
--- rapid.h
+++ rapid.h
@@ -1,11 +1,29 @@
 #ifndef RAPID_H
 #define RAPID_H
 
+#include "magstim.h"
 
-class Rapid
+class Rapid : public MagStim
 {
 public:
-    Rapid();
+    Rapid(QString serialConnection, bool superRapid, QString unlockCode, int voltage, std::tuple<int, int, int> version);
+
+private:
+    int DEFAULT_RAPID_TYPE;
+    int DEFAULT_VOLTAGE;
+    QString DEFAULT_UNLOCK_CODE;
+    bool ENFORCE_ENERGY_SAFETY;
+    std::tuple<int, int, int> DEFAULT_VIRTUAL_VERSION;
+    // MAX_FREQUENCY
+    // JOULES
+
+    bool super;
+    QString unlockCode;
+    int voltage;
+    std::tuple<int, int, int> version;
+    float parameterReturnBytes;
+    bool sequenceValidated;
+    bool repetitiveMode;
 };
 
 #endif // RAPID_H
