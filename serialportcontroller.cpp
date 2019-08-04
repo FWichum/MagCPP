909c05465f8e2257e9a809ea15cec132b324148e
FelixW <35849448+FelixW@users.noreply.github.com>
2019-08-04 15:16:36 +0200

    update run()

--- serialportcontroller.cpp
+++ serialportcontroller.cpp
@@ -36,43 +36,73 @@ void serialPortController::run()
     while (true) {
         //message, reply, readBytes = self._serialWriteQueue.get() //  TODO
         // If the first part of the message is None this signals the process to close the port and stop
-        float message = std::get<QByteArray>(this->serialWriteQueue.front()).toFloat();     // TODO
+        QByteArray bmessage = std::get<QByteArray>(this->serialWriteQueue.front());
+        float message = bmessage.toFloat();
+        QString reply = std::get<QString>(this->serialWriteQueue.front());
+        int readBytes = std::get<int>(this->serialWriteQueue.front());
+        this->serialWriteQueue.pop();
+        char *c;
         // If the first part of the message is None this signals the process to close the port and stop
-        if(message == 0) { // TODO
+        if((int)message == 0) { // TODO
             break;
         }
         //TODO  was für Werte gibt es in Message?
         // If the first part of the message is a 1 this signals the process to trigger a quick fire using the RTS pin
-        if(message == 1) {
+        if((int)message == 1) {
             this->port.setRequestToSend(true);
         }
         // If the first part of the message is a -1 this signals the process to reset the RTS pin
-        else if( message == -1 ){
+        else if((int)message == -1 ){
             this->port.setRequestToSend(false);
         }
         // Otherwise, the message is a command string
         else {
             // There shouldn't be any rubbish in the input buffer, but check and clear it just in case
-            // if(this->port.readBufferSize()!= 0) {           // funktioniert das überhaupt so? wird es benötigt?
-                this->port.clear(QSerialPort::AllDirections);
-
+            // if(this->port.readBufferSize()!= 0) {           // TODO funktioniert das überhaupt so? wird es benötigt?
+            this->port.clear(QSerialPort::AllDirections);
             try {
-                this->port.write("message");
+                // Try writing to the port
+                this->port.write(bmessage);
+                // Read response (this gets a little confusing, as I don't want to rely on timeout to know if there's an error)
                 try {
-                    this->port.read(1);
-                } catch (...) {
-                    this->serialReadQueue.push(1);
+                    this->port.read(c,1);
+                    bmessage.append(c);
+                    if (bmessage.at(0) == 'N') {
+                        while((int) bmessage.back() > 0) {
+                            this->port.read(c,1);
+                            bmessage.append(c);
+                        }
+                        // After the end of the version number, read one more byte to grab the CRC
+                        this->port.read(c,1);
+                        bmessage.append(c);
+                        // If the first byte is not '?', then the message was understood
+                        // so carry on reading in the response (if it was a '?', then this will be the only returned byte).
+                    } else if (bmessage.at(0) != '?') {
+                        // Read the second byte
+                        this->port.read(c,1);
+                        bmessage.append(c);
+                        // If the second returned byte is a '?' or 'S', then the data value supplied either wasn't acceptable ('?') or the command conflicted with the current settings ('S'),
+                        // In these cases, just grab the CRC - otherwise, everything is ok so carry on reading the rest of the message
+                        if (bmessage.at(bmessage.length()-1) != '?' && bmessage.at(bmessage.length()-1) != 'N') {
+                            this->port.read(c,readBytes-2);
+                            bmessage.append(c);
+                        } else {
+                            this->port.read(c,1);
+                            bmessage.append(c);
+                        }
+                        if (reply.toInt()) {
+                            this->serialReadQueue.push(0); // FW: std::make_tuple(0, bmessage);
+                        }
+                    }
+                } catch (...) { // FW: FIXME
+                    this->serialReadQueue.push(serialPortController::SERIAL_READ_ERROR); //FW: TODO tuple
                 }
-            } catch (...) {
-                this->serialReadQueue.push(1);
+            } catch (...) { //FW: FIXME
+                this->serialReadQueue.push(serialPortController::SERIAL_WRITE_ERROR); //FW: TODO tuple
             }
-
-
-
-
         }
     }
-
-
+    // If we get here, it's time to shutdown the serial port controller
+    this->port.close();
     return;
 }
