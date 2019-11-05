f2f69d1b5d05f6bf1fb793c584b223c957d7e866
FelixW <35849448+FelixW@users.noreply.github.com>
2019-11-05 11:19:42 +0100

    Add License and Move includes

--- rapid.cpp
+++ rapid.cpp
@@ -11,12 +11,29 @@
 * Copyright (C) 2019, Hannes Oppermann and Felix Wichum. All rights reserved.
 *
 * GNU General Public License v3.0 (LICENSE)
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 * @brief    Definition of the Rapid class.
 */
 
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
 #include "rapid.h"
-#include "rapid.h"
+
+//*************************************************************************************************************
+
 
 Rapid::Rapid(QString serialConnection, int superRapid, QString unlockCode, int voltage, std::tuple<int, int, int> version) :
     MagStim(serialConnection)
--- serialportcontroller.h
+++ serialportcontroller.h
@@ -11,6 +11,16 @@
 * Copyright (C) 2019, Hannes Oppermann and Felix Wichum. All rights reserved.
 *
 * GNU General Public License v3.0 (LICENSE)
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 * @brief    Contains the declaration of the SerialPortController class.
 *
@@ -25,6 +35,9 @@
 //=============================================================================================================
 
 #include <queue>
+#include <qiodevice.h>
+#include <QIODevice>
+#include <iostream>
 
 //*************************************************************************************************************
 //=============================================================================================================
@@ -36,6 +49,7 @@
 #include <QByteArray>
 #include <QMutexLocker>
 #include <QMutex>
+#include <QSerialPort>
 
 //*************************************************************************************************************
 //=============================================================================================================
